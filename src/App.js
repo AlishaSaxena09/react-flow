1586ce7b84f697dfa074520ffbdbb0e65eb94d3f
alishasaxena <alisha.saxena@mrikal.com>
2024-04-21 22:23:00 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from "react";
+import { useState } from "react";
 
-function App() {
+import "reactflow/dist/style.css";
+import "./App.css";
+import Flow from "./Flow";
+
+export default function App() {
+  const [isText, setIsText] = useState(false);
+
+  const handleText = () => {
+    setIsText(!isText);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center w-screen h-screen py-8">
+      <Flow />
+      <div className="w-1/3">
+        <form className="flex flex-col justify-center gap-4">
+          {isText ? (
+            <div className="flex flex-col justify-center gap-4">
+              <div className="flex items-center px-2 pb-2 border-b text-md border-gray">
+                <span
+                  onClick={handleText}
+                  class="cursor-pointer text-md material-symbols-outlined font-light opacity-60 hover:opacity-40"
+                >
+                  arrow_back
+                </span>
+                <span className="m-auto opacity-60">Message</span>
+              </div>
+              <div className="flex flex-col justify-center gap-4 px-4">
+                <span className="text-xs">Text</span>
+                <textarea
+                  rows="6"
+                  alt="message"
+                  placeholder="Write your text here"
+                  className="p-4 text-sm border"
+                ></textarea>
+              </div>
+            </div>
+          ) : (
+            <div className="px-4">
+              <button
+                onClick={handleText}
+                className="flex flex-col items-center justify-center w-full gap-4 p-8 text-blue-900 border border-blue-900 rounded-lg hover:opacity-60"
+              >
+                <span class="material-symbols-outlined text-blue-900">
+                  chat
+                </span>
+                Message
+              </button>
+            </div>
+          )}
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
